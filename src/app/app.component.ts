4b54ab9ce1f55a6f6a91edf23eddf73db00fdbb0
L0ll094 <62265200+L0ll094@users.noreply.github.com>
2020-09-03 07:23:35 +0200

    First commit, something wrong with app.component

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+/**
+ * @title Card with multiple sections
+ */
+@Component({
+  selector: 'app-root',
+  templateUrl: 'card-fancy-example.html',
+  styleUrls: ['card-fancy-example.css'],
+})
+export class AppComponent {}
