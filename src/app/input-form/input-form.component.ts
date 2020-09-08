cf8ed45c46a107dc9ae6ca2a09f2851875f78d7f
L0ll094 <62265200+L0ll094@users.noreply.github.com>
2020-09-08 07:09:29 +0200

    Setting up form with reactivforms on card

--- src/app/input-form/input-form.component.ts
+++ src/app/input-form/input-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-input-form',
@@ -8,9 +8,25 @@ import { FormControl } from '@angular/forms';
 })
 export class InputFormComponent implements OnInit {
   name= new FormControl('');
-  constructor() { }
+  fluid_properties: FormGroup;
+  emailRegx = /^(([^<>+()\[\]\\.,;:\s@"-#$%&=]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,3}))$/;
 
-  ngOnInit(): void {
+  constructor(
+    private formBuilder: FormBuilder
+  ) {}
+
+  ngOnInit() {
+      this.fluid_properties=this.formBuilder.group({
+      density: [null,[Validators.required, Validators.pattern(this.emailRegx)]],
+      viscosity: [null,Validators.required],
+
+    });
+  }
+  submit(){
+    if(!this.fluid_properties.valid){
+      return;
+    }
+  console.log(this.fluid_properties.value);
   }
 
 }
