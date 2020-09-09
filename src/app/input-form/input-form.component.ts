b374c5ad926f87d81d43801e1fecb53bcfaf4387
L0ll094 <62265200+L0ll094@users.noreply.github.com>
2020-09-09 18:39:06 +0200

    Input form looking better, next step access response

--- src/app/input-form/input-form.component.ts
+++ src/app/input-form/input-form.component.ts
@@ -7,18 +7,16 @@ import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms'
   styleUrls: ['./input-form.component.css']
 })
 export class InputFormComponent implements OnInit {
-  name= new FormControl('');
   fluid_properties: FormGroup;
-  emailRegx = /^(([^<>+()\[\]\\.,;:\s@"-#$%&=]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,3}))$/;
-
+  
   constructor(
     private formBuilder: FormBuilder
   ) {}
 
   ngOnInit() {
       this.fluid_properties=this.formBuilder.group({
-      email: [null,[Validators.required, Validators.pattern(this.emailRegx)]],
-      viscosity: [null,Validators.required],
+      density: [null,[Validators.required,]],
+      Kinematic_Viscosity: [null,Validators.required],
 
     });
   }
