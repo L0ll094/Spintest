cc5997b150e795de3eeb38938eecdd9071a7be4a
L0ll094 <62265200+L0ll094@users.noreply.github.com>
2020-09-22 18:06:02 +0200

    nothing works

--- src/app/input-form/input-form.component.ts
+++ src/app/input-form/input-form.component.ts
@@ -18,10 +18,27 @@ export class InputFormComponent implements OnInit {
       densityParticle: [null,[Validators.required,]],
       densityFeed:[null,[Validators.required,]],
       kinviscosity: [null,Validators.required],
-      spintimes: [null,[Validators.required,]],
-      Nstart: [null,[Validators.required,]],
-      speeds: [null,[Validators.required,]],
-      residualSolids:[null,[Validators.required,]],
+      
+      spintime1: [null,[Validators.required,]],
+      spintime2: [null,[Validators.required,]],
+      spintime3: [null,[Validators.required,]],
+      spintime4: [null,[Validators.required,]],
+
+      Nstart1: [null,[Validators.required,]],
+      Nstart2: [null,[Validators.required,]],
+      Nstart3: [null,[Validators.required,]],
+      Nstart4: [null,[Validators.required,]],
+
+      speed1: [null,[Validators.required,]],
+      speed2: [null,[Validators.required,]],
+      speed3: [null,[Validators.required,]],
+      speed4: [null,[Validators.required,]],
+
+      residualSolids1:[null,[Validators.required,]],
+      residualSolids2:[null,[Validators.required,]],
+      residualSolids3:[null,[Validators.required,]],
+      residualSolids4:[null,[Validators.required,]],
+
       tempSpinTest:[null,[Validators.required,]],
       neededQ: [null,[Validators.required,]],
       
