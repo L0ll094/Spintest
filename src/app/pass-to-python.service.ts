0b3fc33ff7212aea753b043434fcf646f15e0e21
Louise <louise.stjern@alfalaval.com>
2020-10-16 07:27:27 +0200

    Updating the service w datacontainer

--- src/app/pass-to-python.service.ts
+++ src/app/pass-to-python.service.ts
@@ -6,6 +6,21 @@ import { Injectable } from '@angular/core';
 export class PassToPythonService {
 
   constructor() { }
+  dataContainer=[];
+
+  addToContainer(data) {
+    this.dataContainer.push(data);
+  }
+
+  showContents(){
+    return this.dataContainer;
+  }
+
+  clearContainer(){
+    this.dataContainer=[];
+    return this.dataContainer;
+
+  }
 
 
 }
